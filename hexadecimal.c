5ad4f570baa7e2fb651769d9d829011484cab8ec
Muhammed Aydın <112319140+muhammedaydiiinn@users.noreply.github.com>
2023-06-11 12:29:18 +0300

    hexadecimal.c

---
+++ hexadecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int sayi;
+
+    printf("Bir ondalik sayi girin: ");
+    scanf("%d", &sayi);
+
+    printf("Sayi (onaltilik): %X\n", sayi);
+
+    return 0;
+}
